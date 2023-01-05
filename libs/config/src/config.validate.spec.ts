939d6c732f8073e9878bb341362e37cf8192d912
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-05 03:14:30 +0200

    libs/config: test .validateConfig()

--- libs/config/src/config.validate.spec.ts
+++ libs/config/src/config.validate.spec.ts
@@ -1,5 +1,22 @@
+import { IsString } from 'class-validator';
+import { configValidate } from './config.validate';
+
+class SuccessFixture {
+  @IsString()
+  ENV_PROP = 'default';
+}
+
+class FailFixture {
+  @IsString()
+  ENV_PROP: string;
+}
+
 describe('configValidate()', () => {
-  it('???', () => {
-    expect('tests').toBe('here');
-  })
+  it('Should allow config', () => {
+    expect(configValidate(SuccessFixture)).not.toThrowError();
+  });
+
+  it('Should forbid config', () => {
+    expect(configValidate(FailFixture)).toThrowError();
+  });
 });
