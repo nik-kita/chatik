5db80c204544d57734b26b596c333cf106d8741f
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-06 21:41:06 +0200

    start testing sockets

--- apps/chatik-ws/test/e2e/mvp.spec.ts
+++ apps/chatik-ws/test/e2e/mvp.spec.ts
@@ -1,10 +1,12 @@
 import * as req from 'supertest';
 import { faker } from '@faker-js/faker';
+import { WebSocket } from 'ws';
+
 
 // TODO move to fixtures
 const ws = {
   httpHost: `http://localhost:${process.env.TEST_CHATIK_WS_PORT}`,
-  wsHost: '',
+  wsHost: `ws://localhost:${process.env.TEST_CHATIK_WS_PORT}`,
 };
 const auth = {
   host: `http://localhost:${process.env.TEST_CHATIK_AUTH_PORT}`,
@@ -26,6 +28,8 @@ const A = {
 };
 
 describe('MVP', () => {
+  const clients: WebSocket[] = [];
+
   it('Should register new user', async () => {
     const { body: bodyRegister } = await req(auth.host)
       .post(auth.POST['/auth/register'])
@@ -38,15 +42,43 @@ describe('MVP', () => {
       .post(auth.POST['/auth/login'])
       .send(A)
       .expect(201);
-    
+
     expect(bodyLogin).toBeInstanceOf(Object);
 
     const { access, refresh } = bodyLogin;
-    
+
     expect(access).toMatch(/\S+/);
     expect(refresh).toMatch(/\S+/);
 
     A.access = access;
     A.refresh = refresh;
+  });
+
+  it('Should connect to ws', async () => {
+    await new Promise<void>((resolve, reject) => {
+
+      const client = new WebSocket(ws.wsHost, {
+        headers: {
+          Authorization: `Bearer ${A.access}`,
+        },
+      }).on('message', (data) => {
+        console.log(data);
+        resolve();
+      }).on('open', () => {
+        console.log('open');
+        resolve();
+      }).on('error', reject);
+
+      clients.push(client);
+    });
+
+  });
+
+  afterAll(async () => {
+    clients.forEach((c) => {
+      const { readyState, OPEN, CONNECTING } = c;
+
+      if (([OPEN, CONNECTING] as number[]).includes(readyState)) c.close();
+    })
   });
 });
