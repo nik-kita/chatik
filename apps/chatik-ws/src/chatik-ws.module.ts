f71e3a4da59a3c3e0d13f855241d0d86be6b47da
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-06 04:18:38 +0200

    apps/chatik-ws rm unsused

--- apps/chatik-ws/src/chatik-ws.module.ts
+++ apps/chatik-ws/src/chatik-ws.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ChatikWsController } from './chatik-ws.controller';
-import { ChatikWsService } from './chatik-ws.service';
 
 @Module({
   imports: [],
-  controllers: [ChatikWsController],
-  providers: [ChatikWsService],
+  providers: [],
 })
 export class ChatikWsModule {}
--- apps/chatik-ws/src/chatik-ws.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ChatikWsService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
