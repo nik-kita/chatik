572eb3cfd13fbdd790de876aa8a9c80b6597551f
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-06 17:52:06 +0200

    create migration for message

---
+++ pg-migrations/migrations/1673020114855-message-table.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class messageTable1673020114855 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            create table if not exists "message" (
+                message_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+                text varchar not null
+            );
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            drop table if exists "message";
+        `);
+    }
+}
