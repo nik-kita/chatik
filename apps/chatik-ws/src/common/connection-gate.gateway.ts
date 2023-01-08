aadf47742b22170cfe074727688f92308137a12f
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-09 00:27:30 +0200

    error handling for unauth connections

--- apps/chatik-ws/src/common/connection-gate.gateway.ts
+++ apps/chatik-ws/src/common/connection-gate.gateway.ts
@@ -1,23 +1,29 @@
-import { Logger } from '@nestjs/common';
+import { HttpStatus, Logger, UseFilters } from '@nestjs/common';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
+  WebSocketGateway,
   WebSocketServer
 } from '@nestjs/websockets';
 import { Server, WebSocket } from 'ws';
-import { JwtAccessPayload } from '../../../../libs/types/src';
-import { OnlyAuthHandleConnectionService } from './services/only-auth-handle-connection.service';
 import { ConnectedSocketManager } from './services/connected-socket-manager';
+import { OnlyAuthHandleConnectionService } from './services/only-auth-handle-connection.service';
+import { WsExceptionFilter } from './exceptions/ws-exception.filter';
+import { UnAuthConnectionWsException } from './exceptions/unauth-connection.ws-exception';
 
 
 
+
+@WebSocketGateway()
+@UseFilters(WsExceptionFilter)
 export abstract class ConnectionGate implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
   constructor(
     protected onlyAuthGuard: OnlyAuthHandleConnectionService,
     protected connectedSocketManager: ConnectedSocketManager,
     protected logger: Logger,
+    protected wsExceptionFilter: WsExceptionFilter,
   ) { }
 
   @WebSocketServer()
@@ -27,38 +33,39 @@ export abstract class ConnectionGate implements OnGatewayInit, OnGatewayConnecti
     this.logger.debug('.afterInit()');
   }
 
-  // TODO find way to replace this logic to WsExceptionFilter
   async handleConnection(client: WebSocket, firstArg = {}) {
-    let jwtError: Error | null = null;
-    let payload: JwtAccessPayload | null = null;
-
-    // TODO check jwt error codes, define exceptions
     try {
-      payload = await this.onlyAuthGuard.verifyUserFromBearer(firstArg);
-    } catch (error) {
-      jwtError = error;
-    }
+      const payload = await this.onlyAuthGuard.verifyUserFromBearer(firstArg);
 
-    if (jwtError || !payload) {
-      const ERR_CODE = 4003;
-      const errorData = JSON.stringify({
-        error: 'Unauthorized',
-        code: ERR_CODE,
-        reason: jwtError
-          ? String(jwtError)
-          : 'Ws client should have /Authorization/ header with Bearer access token',
+      this.connectedSocketManager.insert(payload.user_id, client);
+      this.logger.debug({
+        event: 'new connection',
+        user_id: payload.user_id,
       });
-      client.send(errorData);
-      client.close(ERR_CODE, errorData);
+    } catch (error) {
+      if (error instanceof UnAuthConnectionWsException) {
+        console.log(error instanceof UnAuthConnectionWsException);
+        console.log(error.name);
+        console.log(error);
+        this.wsExceptionFilter.catch(error, {
+          switchToWs: () => ({
+            getClient: () => client,
+          }),
+        } as any);
 
-      return;
+        return;
+      }
+
+      client.close(HttpStatus.INTERNAL_SERVER_ERROR + 4_000, JSON.stringify({
+        event: 'Error',
+        data: {
+          name: 'Internal server error',
+          message: 'Fail to connect by unknown reason',
+        },
+      }));
+
+      throw error;
     }
-
-    this.connectedSocketManager.insert(payload.user_id, client);
-    this.logger.debug({
-      event: 'new connection',
-      user_id: payload.user_id,
-    });
   }
 
   handleDisconnect(client: any) {
