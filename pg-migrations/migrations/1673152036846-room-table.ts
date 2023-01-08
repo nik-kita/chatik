3938344837854ed3115b580f980652661733b2d0
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-08 06:29:44 +0200

    room-table migration

---
+++ pg-migrations/migrations/1673152036846-room-table.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class roomTable1673152036846 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            create table if not exists "room" (
+                room_id uuid primary key default uuid_generate_v4(),
+
+                type room_type not null
+            );
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            drop table if exists "room";
+        `);
+    }
+
+}
