8e0df9f0fd73748f2c7444f412a1b5c3c1654917
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-11 02:22:58 +0200

    simplify: member table contain flipside logic

--- libs/pg-db/src/entities/member.entity.ts
+++ libs/pg-db/src/entities/member.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { IPgMember } from '../../../types/src/pg';
+import { IPgMember, PgRoomTypeEnum } from '../../../types/src/pg';
 
 
 @Entity({ name: 'member' })
@@ -7,9 +7,18 @@ export class MemberEntity implements IPgMember {
   @PrimaryGeneratedColumn('uuid')
   member_id: string;
 
-  @Column({ type: 'uuid', nullable: false, foreignKeyConstraintName: 'user_user_id' })
+  @Column({ type: 'uuid', nullable: false, foreignKeyConstraintName: 'fk_user_user_id' })
   user_id: string;
 
-  @Column({ type: 'uuid', nullable: false, foreignKeyConstraintName: 'user_user_id' })
+  @Column({ type: 'uuid', nullable: false, foreignKeyConstraintName: 'fk_room_room_id' })
   room_id: string;
+
+  @Column({ type: 'enum', nullable: false, enum: PgRoomTypeEnum })
+  flipside_type: PgRoomTypeEnum;
+
+  @Column({ type: 'uuid', nullable: false, })
+  flipside_id: string;
+
+  @Column({ type: 'uuid', nullable: true, foreignKeyConstraintName: 'fk_flipside_user_user_id' })
+  flipside_user_id: string;
 }
--- libs/types/src/pg/entity-interfaces/pg-member.interface.ts
+++ libs/types/src/pg/entity-interfaces/pg-member.interface.ts
@@ -1,7 +1,14 @@
 import { IPgUser } from './pg-user.interface';
 import { IPgRoom } from './pg-room.interface';
+import { PgRoomTypeEnum } from '../enums/pg-room-type.enum';
 
 export interface IPgMember extends Pick<IPgUser, 'user_id'>,
   Pick<IPgRoom, 'room_id'> {
   member_id: string;
+
+  flipside_id: string;
+
+  flipside_type: PgRoomTypeEnum;
+
+  flipside_user_id: string;
 }
