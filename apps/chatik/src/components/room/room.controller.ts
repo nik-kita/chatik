9d210db3cf50e1c8a8630926dbd01b43588651d6
nik-kita <one.of.these.shoes.isnt.right@gmail.com>
2023-01-11 02:32:35 +0200

    complete rewrite logic from member_flipside to member

--- apps/chatik/src/components/room/room.controller.ts
+++ apps/chatik/src/components/room/room.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateOneToOneRoomReqDto } from '../../../../../libs/dto/src/http';
 import { MemberPgRepo, RoomPgRepo } from '../../../../../libs/pg-db/src';
 import { JwtAccessAuthGuard } from '../../common/strategies/jwt-access/jwt-access-auth.guard';
+import { PgRoomTypeEnum } from '../../../../../libs/types/src/pg';
 
 
 @UseGuards(JwtAccessAuthGuard)
@@ -18,6 +19,28 @@ export class RoomController {
   ): Promise<{
     room_id: string,
   }> {
-    throw new Error('Not implemented yet');
+    const room = await this.memberRepo.getOne({
+      user_id: initiatorUserId,
+      flipside_id: flipsideUserId,
+    });
+
+    if (room) {
+      return {
+        room_id: room.room_id,
+      };
+    } else {
+      const response = await this.roomRepo.insert({ type: PgRoomTypeEnum.ONE_TO_ONE });
+
+      // TODO are you sure that we may not wait until member insertions? (/await/ instead /void/)
+      void this.memberRepo.insertMany([{
+        user_id: initiatorUserId,
+        flipside_id: flipsideUserId,
+        flipside_type: PgRoomTypeEnum.ONE_TO_ONE,
+        flipside_user_id: flipsideUserId,
+        room_id: response.room_id,
+      }]);
+
+      return response;
+    }
   }
 }
